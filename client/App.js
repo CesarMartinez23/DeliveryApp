de15d3aaba370d234e0094f186c45c494a899c3f
CesarMartinez23 <oliver-cesar@hotmail.com>
2023-06-01 22:13:22 -0600

    Adding React Navigation

--- client/App.js
+++ client/App.js
@@ -1,11 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./Navigation/Navigation";
 
 export default function App() {
-  return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <Text className="text-4xl text-orange-500">Welcome to DeliveryApp !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />;
 }
