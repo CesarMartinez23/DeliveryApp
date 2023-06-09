9b3fa303a88cba9a67acbee19be55b52906a28fc
LunaAcosta <lunaacosta300@gmail.com>
2023-06-08 22:32:40 -0600

    Adding Redux Toolkit to the App

--- client/App.js
+++ client/App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./Navigation/Navigation";
+import { Provider } from 'react-redux'
+import { store } from "./Store/Store";
+
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
